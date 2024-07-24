7099f799ada9f646ba917ad4364f8bc30ec24900
Zwanez <reyes_milven@yahoo.com>
2024-07-25 02:54:29 +0800

    First commit

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+import pandas as pd
+
+st.title('Airbnb Data Explorer')
+st.write('Welcome to the Airbnb Data Explorer. This app allows you to explore Airbnb data from major cities around the world.')
+
