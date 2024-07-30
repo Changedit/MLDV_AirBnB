156553b73aa8cf29cc5329f2026ae85166fe3b2e
Zwanez <56713402+Changedit@users.noreply.github.com>
2024-07-30 12:17:07 +0800

    Update app.py

--- app.py
+++ app.py
@@ -77,5 +77,5 @@ st.write(input_features)
 
 if st.button('Predict Price'):
     
-    prediction = predict(input_features) * 33
+    prediction = predict(input_features) * 1
     st.success(f'The predicted price is ${prediction:.2f}')
